037a7034b91eb307187daa1febcae616544dd5bc
Caíque Oliveira <caique_oliveira@estudante.sc.senai.br>
2025-10-14 20:08:51 -0300

    Implementando formas de pagamento

--- src/main/java/tecdes/pagamento/view/FormPagamento.java
+++ src/main/java/tecdes/pagamento/view/FormPagamento.java
@@ -11,9 +11,13 @@ public class FormPagamento extends JFrame{
     private JComboBox<String> cmbMetodo;
     private JTextArea txtResultado;
     private ControllerPagamento controller;
+    JLabel lblTipoCartao;
+    JTextField txtParcelas;
+    JComboBox<String> cmbTipoCartao;
+    JLabel lblParcelas;
 
     public FormPagamento(){
-        setSize(420, 340);
+        setSize(420, 520);
         setTitle("Gestor de Pagamentos - v1.0");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
@@ -25,6 +29,7 @@ public class FormPagamento extends JFrame{
 
     private void initComponents(){
 
+        // Entrada de Dados ------------------------------------------
         JLabel lblValor = new JLabel("Valor (R$):");
         lblValor.setBounds(40, 30, 100, 25);
         add(lblValor);
@@ -44,9 +49,35 @@ public class FormPagamento extends JFrame{
         cmbMetodo.setBounds(180, 70, 170, 25);
         add(cmbMetodo);
 
+        lblTipoCartao = new JLabel("Tipos de Cartão: ");
+        lblTipoCartao.setBounds(40, 110, 120, 25);
+        lblTipoCartao.setVisible(false);
+        add(lblTipoCartao);
+
+        cmbTipoCartao = new JComboBox<>(new String[]{"Débito", "Crédito"});
+        cmbTipoCartao.setBounds(160, 110, 190, 25);
+        cmbTipoCartao.setVisible(false);
+        add(cmbTipoCartao);
+
+        lblParcelas = new JLabel("Parcelas: ");
+        lblParcelas.setBounds(40, 150, 120, 25);
+        lblParcelas.setVisible(false);
+        add(lblParcelas);
+
+        txtParcelas = new JTextField();
+        txtParcelas.setBounds(160, 150, 50, 25);
+        txtParcelas.setVisible(false);
+        add(txtParcelas);
+        
+        // Botões ---------------------------------------------------
+
         JButton btnProcessar = new JButton("Processar Pagamento");
-        btnProcessar.setBounds(120, 120, 180, 35);
+        btnProcessar.setBounds(120, 200, 180, 35);
         add(btnProcessar);
+
+        JButton btnSalvarHistorico = new JButton("Salvar Histórico");
+        btnSalvarHistorico.setBounds(120, 240, 180, 35);
+        add(btnSalvarHistorico);
 
         txtResultado = new JTextArea();
         txtResultado.setEditable(false);
@@ -54,10 +85,34 @@ public class FormPagamento extends JFrame{
         txtResultado.setWrapStyleWord(true);
 
         JScrollPane scroll = new JScrollPane(txtResultado);
-        scroll.setBounds(40, 180, 320, 100);
+        scroll.setBounds(40, 290, 320, 100);
         add(scroll);
 
+        // Eventos -------------------------------------------------
         btnProcessar.addActionListener(e -> Processar());
+        cmbMetodo.addActionListener (e -> atualizarCampos());
+        cmbTipoCartao.addActionListener(e -> atualizarParcelas());
+        }
+
+    private void atualizarCampos(){
+        String metodo = (String) cmbMetodo.getSelectedItem();
+
+        boolean isCartao = metodo.equals("Cartão");
+
+        lblTipoCartao.setVisible(isCartao);
+        cmbTipoCartao.setVisible(isCartao);
+
+        lblParcelas.setVisible(false);
+        txtParcelas.setVisible(false);
+        txtParcelas.setText("");
+    }
+
+    private void atualizarParcelas(){
+        String tipo = (String) cmbTipoCartao.getSelectedItem();
+        boolean isCredito = tipo.equals("Crédito");
+
+        lblParcelas.setVisible(isCredito);
+        txtParcelas.setVisible(isCredito);
     }
 
     private void Processar(){
